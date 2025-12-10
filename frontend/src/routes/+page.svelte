9feddcd0d06dd27929d1159996d02278d5e7e5bb
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-12-10 16:08:41 +0100

    Installed the packages again

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	console.log('This is the +page.svelte file');
+</script>
+
+<div>
+	<h1>I Like fish</h1>
+</div>
