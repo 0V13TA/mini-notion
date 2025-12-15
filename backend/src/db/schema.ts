d668c2a171314eed3965fcf78422784c97692e77
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-12-15 15:24:57 +0100

    Added a pages table.

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,4 +1,4 @@
-import { uuid, text, timestamp, pgTable } from "drizzle-orm/pg-core"
+import { uuid, text, timestamp, pgTable, jsonb } from "drizzle-orm/pg-core"
 
 export const profiles = pgTable("profiles", {
   id: uuid("id").primaryKey().notNull(),
@@ -8,3 +8,12 @@ export const profiles = pgTable("profiles", {
   updatedAt: timestamp("updated_at").defaultNow(),
   profile_picture: text("profile_picture").default("https://placehold.co/300x300"),
 });
+
+export const pages = pgTable("pages", {
+  id: uuid("id").primaryKey().notNull(),
+  userId: uuid("user_id").notNull().references(() => profiles.id),
+  title: text("title").default("Untitled Page").notNull(),
+  icon: text("icon"),
+  content: jsonb("content").default("[]").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+})
